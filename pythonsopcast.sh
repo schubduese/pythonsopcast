2d47113fd5c60e800e69b4b41b60ad7af165af7e
root <root@thinkpad.triebwerk>
2014-09-02 21:51:40 +0200

    startSopcast

--- pythonsopcast.sh
+++ pythonsopcast.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+
 dateiname="pythonsopcast.conf"
 sopcastPath=""
 localPort=""
@@ -15,6 +17,8 @@ def readConfig():
             sopcastPath=zeile.split("=")[1]
         if zeile.startswith("localport"):
             localPort=zeile.split("=")[1]
+        if zeile.startswith("playerport"):
+            playerPort=zeile.split("=")[1]
         if zeile.startswith("channel"):
             s = zeile.split("=")[1]
             channelList.append((s.split(",")[0],s.split(",")[1]))
@@ -26,6 +30,21 @@ def readConfig():
     for channel in channelList:
         print "Channel" + channel[0] + ":: URL:" + channel[1]
             
+def testConfigParameter():
+    if not (sopcastPath) or not(localPort) or not (playerPort):
+        print "nein"
 
-    
+def startSopcast():
+    channelNummer = -1
+    print "Channelnummer wählen"
+    for channel in channelList:
+        channelNummer = channelNummer +1
+        print "[" + str(channelNummer) +"] "+  channel[0]
+    channelNummer = input()
+    print "sadf" + sopcastPath
+    aufruf=sopcastPath +" " + channelList[channelNummer][1] + " " + localPort + " " + playerPort
+    print aufruf
+    os.system(aufruf)
+
 readConfig()
+startSopcast()
